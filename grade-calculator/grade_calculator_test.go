3aad154fa0fc17303c3179b962df4093ef16df6b
dehaarte <jahdehaarte@gmail.com>
2025-10-02 22:21:10 -0400

    Added the test: TestGetGradeC, TestGetGradeD, and TestGradeType to ensure 100% code coverage
    Added TestGetGradeC to test grades with the expected value of  C, TestGetGradeD to test grades with the expected  value of D, and TestGradeType to verify GradeType  returns the correct string values: 'assignment', 'exam', and 'essay'.

--- grade-calculator/grade_calculator_test.go
+++ grade-calculator/grade_calculator_test.go
@@ -34,6 +34,38 @@ func TestGetGradeB(t *testing.T) {
 	}
 }
 
+func TestGetGradeC(t *testing.T) {
+	expected_value := "C"
+
+	gradeCalculator := NewGradeCalculator()
+
+	gradeCalculator.AddGrade("open source assignment", 70, Assignment)
+	gradeCalculator.AddGrade("exam 1", 79, Exam)
+	gradeCalculator.AddGrade("essay on ai ethics", 75, Essay)
+
+	actual_value := gradeCalculator.GetFinalGrade()
+
+	if expected_value != actual_value {
+		t.Errorf("Expected GetGrade to return '%s'; got '%s' instead", expected_value, actual_value)
+	}
+}
+
+func TestGetGradeD(t *testing.T) {
+	expected_value := "D"
+
+	gradeCalculator := NewGradeCalculator()
+
+	gradeCalculator.AddGrade("open source assignment", 64, Assignment)
+	gradeCalculator.AddGrade("exam 1", 67, Exam)
+	gradeCalculator.AddGrade("essay on ai ethics", 68, Essay)
+
+	actual_value := gradeCalculator.GetFinalGrade()
+
+	if expected_value != actual_value {
+		t.Errorf("Expected GetGrade to return '%s'; got '%s' instead", expected_value, actual_value)
+	}
+}
+
 func TestGetGradeF(t *testing.T) {
 	expected_value := "F"
 
@@ -47,5 +79,17 @@ func TestGetGradeF(t *testing.T) {
 
 	if expected_value != actual_value {
 		t.Errorf("Expected GetGrade to return '%s'; got '%s' instead", expected_value, actual_value)
+	}
+}
+
+func TestGradeType(t *testing.T) {
+	if Assignment.String() != "assignment" {
+		t.Errorf("Expected Assignment to return 'assignment'; got '%s' instead", Assignment.String())
+	}
+	if Exam.String() != "exam" {
+		t.Errorf("Expected Exam to return 'exam'; got '%s' instead", Exam.String())
+	}
+	if Essay.String() != "essay" {
+		t.Errorf("Expected Essay to return 'essay'; got '%s' instead", Essay.String())
 	}
 }
